89dec2bfdad7b32150a61de829c8a93d529ab63d
Spencer <spencer112233@hotmail.co.uk>
2018-12-02 22:39:32 +0000

    Adding some UI stuff

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -6,7 +6,6 @@ import DelayLink from '../components/DelayLink.js'
 import '../styles/Home.css';
 
 const insideStyles = {
-  padding: 20,
   position: "absolute",
   top: "50%",
   left: "50%",
@@ -24,19 +23,39 @@ class Home extends Component {
   render() {
     return (
       <div className="App">
-        <Animated animationIn="fadeIn" animationOut="bounceOutDown" isVisible={this.state.isVisible}>
+        <Parallax
+          bgImage={require("../static/images/background.jpg")}
+          strength={200}
+          renderLayer={percentage => (
+            <div>
+                <div style={{
+                  position: "absolute",
+                  left: "50%",
+                  top: "60%",
+                  borderRadius: "50%",
+                  opacity: percentage > 0.6 ? 1 - ((percentage - 0.6)*4): 1,
+                }} class="rectangle"></div>
+                <img src={require("../static/images/wheel.png")} height={20} width={20}
+                style={{
+                  position: "absolute",
+                  left: "50%",
+                  top: "60%",
+                  borderRadius: "50%",
+                  transform: "translate(-50%,-50%) rotate(" + (percentage > 0.6 ? percentage * 490 : 0) + "deg)",
+                  opacity: percentage > 0.6 ? 1 - ((percentage - 0.6)*4): 1,
+                }}/>
+            </div>
+          )}
+        >
+          <div style={{ height: 500 }}>
+            <div style={insideStyles}>Spencer's Website</div>
+          </div>
+        </Parallax>
+        <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={this.state.isVisible}>
           <Parallax bgImage={require("../static/images/background.jpg")} strength={500}>
             <div style={{ height: 500 }}>
               <div style={insideStyles}>
                 <DelayLink delay={800} onDelayStart={this.setter} to='/gitrepos'>Git Repos!</DelayLink>
-              </div>
-            </div>
-          </Parallax>
-          <h1>| | |</h1>
-          <Parallax bgImage={require("../static/images/background.jpg")} strength={500}>
-            <div style={{ height: 500 }}>
-              <div style={insideStyles}>
-                <DelayLink delay={800} to='/gitrepos'>Git Repos!</DelayLink>
               </div>
             </div>
           </Parallax>
