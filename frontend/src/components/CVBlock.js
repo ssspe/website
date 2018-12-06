ac3e9a5ac568c26d644354aae3288961df092a52
Spencer Robertson <spencer.robertson@zirconsoftware.co.uk>
2018-12-06 09:53:31 +0000

    Need to tidy up but added animation to collapsable list

--- frontend/src/components/CVBlock.js
+++ frontend/src/components/CVBlock.js
@@ -11,12 +11,13 @@ import _ from 'lodash';
 class CVBlock extends Component {
   state = {
     isVisible: true,
+    class: 'closed__arrow',
+    isOpen: false
   };
 
   renderWorkExperienceItem = (work) => {
     return(
       <TimelineItem
-        key="001"
         dateText={work.dateFrom + '-' + work.dateTo}
         dateInnerStyle={{ background: '#282c34', color: '#FFF' }}
         bodyContainerStyle={{
@@ -37,14 +38,17 @@ class CVBlock extends Component {
   }
 
   handleClick = (e) => {
-    console.log("Spenc");
+    this.setState({isOpen: !this.state.isOpen})
+    if (this.state.isOpen) {
+      this.setState({class: 'closed__arrow'})
+    } else {
+      this.setState({class: 'open__arrow'})
+    }
   }
 
   renderEducationItem = (education) => {
-    console.log(education);
     return(
         <TimelineItem
-          key="001"
           dateText={education.dateFrom + '-' + education.dateTo}
           dateInnerStyle={{ background: '#282c34', color: '#FFF' }}
           bodyContainerStyle={{
@@ -58,7 +62,8 @@ class CVBlock extends Component {
           <h3>{education.institute}</h3>
           <h4>{education.level}</h4>
           <p id="toggler" onClick={this.handleClick}>
-            > Grades
+            <img className={this.state.class} src={require("../static/images/arrow.png")} height={20} width={20}/>
+            Grades
           </p>
           <UncontrolledCollapse toggler="#toggler">
             <Card>
