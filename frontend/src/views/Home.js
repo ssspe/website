7c05315d031ebd29fe262369617571ad5e55c8ee
Spencer Robertson <spencer.robertson@zirconsoftware.co.uk>
2018-12-12 15:09:01 +0000

    Started addign SVG animations

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -4,11 +4,12 @@ import { Parallax } from 'react-parallax';
 import '../styles/Home.css';
 import Particles from 'react-particles-js';
 import { Element, scroller } from 'react-scroll';
+import Vivus from "vivus";
 
 const particleParams = {
   particles: {
     number: {
-      value: 300
+      value: 150
     },
     size: {
       value: 5,
@@ -16,6 +17,12 @@ const particleParams = {
     },
     opacity: {
       value: 0.5
+    },
+    move: {
+        random: true,
+        speed: 1,
+        direction: "out",
+        out_mode: "out"
     }
   },
   interactivity: {
@@ -35,7 +42,8 @@ const particleParams = {
       },
       bubble: {
         distance: 150,
-        size: 10
+        size: 5,
+        opacity: 1,
       },
       repulse:{
         distance: 100
@@ -52,6 +60,10 @@ class Home extends Component {
     };
   }
 
+  componentDidMount(){
+        new Vivus('my-div', {duration: 200, file: require('../static/images/download.svg')});
+  }
+
   scrollTo() {
     scroller.scrollTo('scroll-to-element', {
       duration: 1000,
@@ -62,14 +74,14 @@ class Home extends Component {
 
   render() {
     const background = require("../static/images/background.jpg");
-
     return (
       <div className="App">
+
         <Particles
           className="particle"
           canvasClassName="particle"
           params={ particleParams }/>
-        <div className="home__title inside__style">Spencer's Website</div>
+        <div id="my-div" className="home__title inside__style" style={{ height: 100 }}></div>
         <div className="home__border">
           <img className="navigate__arrow" onClick={this.scrollTo} src={require("../static/images/arrow.png")} alt="Wheel" />
         </div>
